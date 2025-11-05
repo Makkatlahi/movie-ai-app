eba159cfc3d19b51f81fb7ce256dd4147e4778ff
Sean Mafnas <seanvincent671@gmail.com>
2025-11-06 09:14:12 +1000

    refactor: improve comments and log movie data in index.js

--- config.js
+++ config.js
@@ -1,9 +1,11 @@
 import OpenAI from "openai";
 import { createClient } from "@supabase/supabase-js";
 
-/** OpenAI config */
+/*If api key is available and correctly configured */
 if (!import.meta.env.VITE_OPENAI_API_KEY)
   throw new Error("OpenAI API key is missing or invalid.");
+
+/** OpenAI config */
 export const openai = new OpenAI({
   apiKey: import.meta.env.VITE_OPENAI_API_KEY,
   dangerouslyAllowBrowser: true,
--- index.js
+++ index.js
@@ -1,1 +1,24 @@
+/* 
+
+We need to fetch the movies data, insert them into supabase, 
+call the openai api and create vector embeddings for the data, 
+then use openai to spit back the results requested by the user. 
+
+*/
 import { openai, supabase } from "./config.js";
+import movies from "./content.js";
+
+// main("The quick brown fox jumped over the lazy dog"); // test input
+
+// Need to insert to supabase
+
+// calling the openai api to create vector embeddings for the input
+async function main(input) {
+  const embedding = await openai.embeddings.create({
+    model: "text-embedding-ada-002",
+    input,
+  });
+  console.log(embedding.data[0].embedding);
+}
+
+// get user input
